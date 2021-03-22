f5500a0f8a1733c1be167635fafaf4e332681d0d
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-03-22 18:17:45 +0100

    Inserita funzione con filter

--- snack-3/js/snack3.js
+++ snack-3/js/snack3.js
@@ -2,7 +2,7 @@
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 */
-//ES5
+//ES 6 let const
 /*
 function arrayInterval( array, a, b) {
   var arrayFunc = [];
@@ -19,17 +19,22 @@ const newListNumber = arrayInterval( listNumber, 2, 6);
 console.log(newListNumber);
 */
 //ES6 filter
-var min = parseInt(prompt('Inserisci un numero tra 1 e 8'));
-var max = parseInt(prompt('Inserisci un numero tra 1 e 8'));
+function arrayInterval(array, min, max) {
+  return array.filter((item, index) => index >= min && index <= max);
+}
 
-
-
+var min = parseInt(prompt('Inserisci un numero tra 1 e 7'));
+var max = parseInt(prompt('Inserisci un numero tra 1 e 7'));
 const list = [1,2,3,4,5,6,7,8];
 
-//const list2 = newList(1, 7, list);
 
+/*
 const list2 = list.filter((item, index) => {
   return  item = min && item <= max;
 });
+*/
+
+const list2 = arrayInterval(list, min, max);
+
 
 console.log(list2);
